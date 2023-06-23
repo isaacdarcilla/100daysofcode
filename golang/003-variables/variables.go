bceeee394c2e10014c234098fb9448708b86f461
Isaac A <isaacdarcilla@gmail.com>
2023-06-23 08:43:11 +0800

    go: variables

---
+++ golang/003-variables/variables.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 1
+	fmt.Println(i)
+	
+	var name string = "Isaac"
+	fmt.Println(name)
+
+	var is_string = false;
+	fmt.Println(is_string)
+
+	last_name := "Arcilla"
+	fmt.Println(last_name)
+
+	var f, m = true, "Male";
+	var num1, num2 int = 1, 3
+
+	fmt.Println(f, m)
+	fmt.Println(num1, num2)
+
+	var e int
+	fmt.Println(e)
+}
